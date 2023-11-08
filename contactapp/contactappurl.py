b6024c28c1a761ef5c087cd3a9bf870f1ae0fc81
jincy_joy <jincyjj1996@gmail.com>
2023-11-08 09:34:13 +0530

    Commit message

--- contactapp/contactappurl.py
+++ contactapp/contactappurl.py
@@ -1,10 +1,13 @@
 from django.urls import path,re_path
 from .import views
+from .views import contact_list
 
 urlpatterns=[
 
        path('page2/', views.page2,name='page2'),
        path('createcontact/', views.createcontact,name='createcontact'),
+       path('contact_list/', contact_list, name='contact_list'),
+
 
 
 
--- contactapp/views.py
+++ contactapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Contact
 # Create your views here.
 def page2(request):
     return render(request, "contact/page2.html")
@@ -7,3 +7,8 @@ def page2(request):
 def createcontact(request):
     return render(request, "contact/createcontact.html")
 
+
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contact/contactlist.html', {'contacts': contacts})
